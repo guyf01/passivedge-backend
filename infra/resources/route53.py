6459848005946e51353819ca729087494c654b97
Guy Fried <109249411+guyf01@users.noreply.github.com>
2025-12-26 00:21:14 +0200

    moved-api (#16)

--- infra/resources/route53.py
+++ infra/resources/route53.py
@@ -1,12 +1,11 @@
 """Route 53 configuration for DNS."""
 
 from aws_cdk.aws_route53 import HostedZone
-from aws_cdk.aws_certificatemanager import Certificate, CertificateValidation
 from constructs import Construct
 
 
 class Route53Zone(Construct):
-    """Route 53 hosted zone lookup and ALIAS record."""
+    """Route 53 hosted zone lookup."""
 
     def __init__(self, scope: Construct, id: str):
         super().__init__(scope, id)
@@ -25,9 +24,3 @@ class Route53Zone(Construct):
             domain_name=self.domain_name,
         )
 
-        # Create ACM certificate with DNS validation
-        self.certificate = Certificate(
-            self, "Certificate",
-            domain_name=self.domain_name,
-            validation=CertificateValidation.from_dns(self.hosted_zone),
-        )
